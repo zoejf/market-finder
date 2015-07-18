d7a5cf431ee9e9015028339141515814f9c1f3f0
zoejf <zoejfoss@gmail.com>
2015-07-18 16:04:19 -0700

    test data renders to page from server.js

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,27 +5,18 @@ $(function() {
     //html element to hold all results
     var $resultsList = $("#results-list");
 
-    var markets = [
-    	{name: "Inner Richmond", 
-    	location: "Clement St", 
-    	day: "Sunday", 
-    	hours: "7am - 2pm" }, 
-    	{name: "Ferry Building",
-    	 location: "Embarcadero", 
-    	 day: "Wednesday", 
-    	 hours: "12pm - 4pm"}, 
-    	 {name: "Heart of the City",
-    	  location: "UN Plaza", 
-    	  day: "Wednesday", 
-    	  hours: "7:30am - 5:30pm"}
-    ]
-
-    //compiles template to the page
-    _.each(markets, function(result) {
-      var $listItem = $(resultsTemplate(result));
-    	$resultsList.append($listItem)
-    });
     
+    
+    //render existing data to the page
+    $.get('/api/markets', function(data) {
+        var markets = data;
+
+        //renders template to the page
+        _.each(markets, function(result) {
+          var $resultItem = $(resultsTemplate(result));
+            $resultsList.append($resultItem)
+        });
+    })
 
 
 });
--- server.js
+++ server.js
@@ -10,9 +10,30 @@ app.use(express.static(__dirname + '/public'));
 // tell app to use bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
-// set up root route to respond with 'hello world'
+  var markets = [
+    	{name: "Inner Richmond", 
+    	location: "Clement St", 
+    	day: "Sunday", 
+    	hours: "7am - 2pm" }, 
+    	{name: "Ferry Building",
+    	 location: "Embarcadero", 
+    	 day: "Wednesday", 
+    	 hours: "12pm - 4pm"}, 
+    	 {name: "Heart of the City",
+    	  location: "UN Plaza", 
+    	  day: "Wednesday", 
+    	  hours: "7:30am - 5:30pm"}
+    ]
+//STATIC ROUTES
+// root route to get to main page
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/views/index.html');
+});
+
+//API ROUTES
+
+app.get('/api/markets', function (req, res) {
+	res.json(markets);
 });
 
 // listen on port 3000
