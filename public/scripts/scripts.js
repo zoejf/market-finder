9b816946bd17aa12a455fa29c4284f44f621ac40
zoejf <zoejfoss@gmail.com>
2015-07-20 17:07:26 -0700

    searchID and searchZip work

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,7 +7,7 @@ $(function() {
     var $resultsList = $('#results-list');
     var $detailsPanel = $('#market-details');
 
-    var marketResults = [];
+    // var marketResults = [];
     
     //results list: render test data to the page
     $.get('/api/markets', function(data) {
@@ -20,6 +20,63 @@ $(function() {
         });
     });
 
+//DEFINE FUNCTIONS FOR USDA API 
+    //get all results with that zip code 
+    var searchZip = function (zip) {
+        $.ajax({
+            type: 'GET', 
+            url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zip, 
+            success: function (data) {
+                // console.log('data: ', data);
+                //push each of the found results to my temp marketResults array
+                var marketResults = data.results;
+                // _.each(results, function(el) {
+                //     marketResults.push(el);
+                // });
+                console.log('market results in function: ', marketResults);
+                searchId(marketResults);
+            }
+        })
+        // console.log('market results in function: ', marketResults);
+        // return marketResults;
+    };
+
+    //search by id of each of the items in an array
+    var searchId = function(marketResults) {
+       
+        //iterate through each item in marketResults array
+        for (i = 0; i < marketResults.length; i ++) {
+             //define id for each of the objects in marketResults array
+             var id = parseInt(marketResults[i].id);
+             console.log('marketResults[i].id', id);
+
+             var marketResult = marketResults[i];
+             console.log('market result: ', marketResult);
+             //get details for the item with that id
+             $.ajax({
+                 type: 'GET',
+                 url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + id,
+                 success: function (data) {
+                    console.log('id data: ', data);
+                    console.log('market result inside success', marketResult);
+                    marketResult.address = data.marketdetails.Address;
+                    marketResult.google = data.marketdetails.GoogleLink;
+                    marketResult.products = data.marketdetails.Products;
+                    marketResult.schedule = data.marketdetails.Schedule;
+                    console.log('marketResults after new keys: ', marketResult);
+                    appendResults(marketResult);  // will work after fixing underscore template
+                 }
+             })
+        };
+    };
+
+    var appendResults = function (marketResults) {
+
+    };
+
+// ! end of defining functions
+
+
     //on click of the 'Go' button
     $('#search-zip').on('submit', function(event) {
         event.preventDefault();
@@ -27,29 +84,12 @@ $(function() {
         var zip = $('#zipcode').val();
         console.log('zipcode: ', zip);
 
-        //get all results with that zip code from form
-        $.ajax({
-            type: 'GET', 
-            url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zip, 
-            success: function (data) {
-                console.log('data: ', data);
-                //push each of the found results to my temp marketResults array
-                var results = data.results;
-                _.each(results, function(el) {
-                    marketResults.push(el);
-                });
-                console.log(marketResults);
-            }
-        })
+        //search by specific zip entered in form
+        searchZip(zip);
+        // console.log('market results 2: ', marketResults);
 
-   
-       //iterate through each item in marketResults array
-
-            //get details for the item with that id
-
-            //push the new details into the existing object
-
-
+        //search by each id from zip results
+        
 
         //for each item in marketResults array, append data to html page
 
@@ -60,6 +100,7 @@ $(function() {
 
     //on click of the info icon, find that market's details
     $resultsList.on('click', '.glyphicon-info-sign', function (event) {
+
             //find the market's id
             var marketId = $(this).closest('.market').attr('data-id');
             // console.log('marketId: ' + marketId);
